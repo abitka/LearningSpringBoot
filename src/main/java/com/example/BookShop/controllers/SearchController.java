0d7c411c3c57616bef54d49a64e91311988c4d48
abitka <franzuz89gmail.com>
2021-06-23 16:37:05 +0700

    add missing pages controllers

--- src/main/java/com/example/BookShop/controllers/SearchController.java
+++ src/main/java/com/example/BookShop/controllers/SearchController.java
@@ -2,7 +2,9 @@ package com.example.BookShop.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/search")
@@ -10,6 +12,12 @@ public class SearchController {
 
     @GetMapping
     public String search() {
-        return "not found";
+        return "index";
+    }
+
+    @PostMapping
+    public String search(@RequestParam String value) {
+        System.out.println(">>>>>> search: " + value);
+        return "index";
     }
 }
