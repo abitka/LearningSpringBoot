f4667cc1cb34524af35d8c21d32eb7923f145355
abitka <franzuz89gmail.com>
2021-06-29 14:22:30 +0700

    not need

--- src/main/java/com/example/BookShop/repositories/TestEntityCRUDRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.BookShop.repositories;
-
-import com.example.BookShop.entity.TestEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TestEntityCRUDRepository extends CrudRepository<TestEntity, Long> {
-
-}
