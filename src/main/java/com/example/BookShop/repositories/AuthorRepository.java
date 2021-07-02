f7956b08c52fa6b261c86c02e153b16e3e7d5ec7
abitka <franzuz89@gmail.com>
2021-07-02 14:01:39 +0700

    add repository annotation

--- src/main/java/com/example/BookShop/repositories/AuthorRepository.java
+++ src/main/java/com/example/BookShop/repositories/AuthorRepository.java
@@ -2,7 +2,9 @@ package com.example.BookShop.repositories;
 
 import com.example.BookShop.entity.AuthorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AuthorRepository extends JpaRepository<AuthorEntity, Integer> {
 
 }
--- src/main/java/com/example/BookShop/repositories/BookRepository.java
+++ src/main/java/com/example/BookShop/repositories/BookRepository.java
@@ -3,9 +3,11 @@ package com.example.BookShop.repositories;
 import com.example.BookShop.entity.book.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BookRepository extends JpaRepository<BookEntity, Integer> {
 
     @Query("from BookEntity")
